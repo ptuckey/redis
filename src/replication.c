2b4ecedee8c33126fc86d54fb90f2d8843e4d9c1
Manish Singh <yosh@gimp.org>
2012-09-18 11:19:51 -0700

    Fix merge screwup

--- src/replication.c
+++ src/replication.c
@@ -427,21 +427,6 @@ void readSyncBulkPayload(aeEventLoop *el, int fd, void *privdata, int mask) {
                 server.repl_transfer_last_fsync_off, sync_size);
             server.repl_transfer_last_fsync_off += sync_size;
         }
-
-        /* Check if the transfer is now complete */
-        if (server.repl_transfer_read == server.repl_transfer_size) {
-            if (rename(server.repl_transfer_tmpfile,server.rdb_filename) == -1) {
-                redisLog(REDIS_WARNING,"Failed trying to rename the temp DB into dump.rdb in MASTER <-> SLAVE synchronization: %s", strerror(errno));
-                replicationAbortSyncTransfer();
-                return;
-            }
-            server.repl_transfer_lastio = server.unixtime;
-            if (write(server.repl_transfer_fd,buf,nread) != nread) {
-                redisLog(REDIS_WARNING,"Write error or short write writing to the DB dump file needed for MASTER <-> SLAVE synchronization: %s", strerror(errno));
-                goto error;
-            }
-            server.repl_transfer_read += nread;
-        }
     }
 
     /* Check if the transfer is now complete */
